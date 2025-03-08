76fb23744fb61338f69672162b3d346f001a8814
xyryc <amdanikzz@gmail.com>
2025-03-08 20:08:54 +0600

    added cloud icon+doc upload field

--- src/layouts/PageContent.tsx
+++ src/layouts/PageContent.tsx
@@ -1,4 +1,4 @@
-import { Info, Sparkles } from "lucide-react";
+import { Info, Sparkles, X } from "lucide-react";
 import BackgroundPattern from "/hero_pattern.png";
 import DragNDropIcon from "/dragndrop.png";
 import UrlIcon from "/url.png";
@@ -13,6 +13,10 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../components/ui/tooltip";
+import CloudIcon from "/cloud.png";
+import PdfIcon from "/pdfIcon.png";
+import DocIcon from "/docIcon.png";
+import { Checkbox } from "../components/ui/checkbox";
 
 const PageContent = () => {
   return (
@@ -23,9 +27,9 @@ const PageContent = () => {
         alt="background pattern"
       />
 
-      <div className="flex flex-col items-center justify-center h-full px-4">
+      <div className="flex flex-col items-center justify-center h-full px-4 2xl:pt-10">
         {/* info */}
-        <div className="flex items-center gap-3 mt-4 2xl:mt-10 bg-[#242424] pl-4 pr-3 py-2 rounded-full">
+        <div className="flex items-center gap-3 mt-10 bg-[#242424] pl-4 pr-3 py-2 rounded-full">
           <Info className="text-[#FB9937]" />
           <p className="text-xs 2xl:text-sm">
             The web version does not display local chats. To access all
@@ -38,11 +42,19 @@ const PageContent = () => {
         </div>
 
         {/* main content */}
-        <div className="my-8 3xl:my-20">
+        <div className="my-12 2xl:my-20">
           {/* add files */}
           <div className="px-4">
-            <p className="text-5xl 2xl:text-[54px] font-medium mb-6">
-              Add Files to Cloud Chat
+            <p className="text-5xl 2xl:text-[54px] font-medium mb-6 flex justify-center">
+              Add Files to{" "}
+              <span className="hidden md:block mx-4 text-sm p-4 bg-[#1E1E1E] rounded-2xl -rotate-12 -mt-6">
+                <img
+                  className="size-11 object-scale-down"
+                  src={CloudIcon}
+                  alt="Cloud Icon"
+                />
+              </span>{" "}
+              Cloud Chat
             </p>
             <p className="text-xs 2xl:text-sm">
               Your files will not be stored on our servers and no AI models will
@@ -54,52 +66,94 @@ const PageContent = () => {
           </div>
 
           {/* cards */}
-          <div className="max-w-[820px] px-4 py-4 2xl:py-[60px] grid grid-cols-1 sm:grid-cols-2 gap-2 text-left">
-            {/* drag and drop card */}
-            <div className="px-6 py-4 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
-              <img src={DragNDropIcon} alt="drag and drop icon" />
+          <div
+            className="max-w-[820px] px-4 py-4 2xl:py-[20px]"
+            style={{
+              "@media (min-width: 1920px)": {
+                padding: "60px", 
+              },
+            }}
+          >
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 text-left">
+              {/* drag and drop card */}
+              <div className="px-6 py-2.5 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+                <img src={DragNDropIcon} alt="drag and drop icon" />
 
-              <p className="2xl:text-lg font-semibold">
-                Drag & drop local files here, or click to select
-              </p>
-            </div>
-
-            {/* add url card */}
-            <div className="px-6 py-4 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
-              <img src={UrlIcon} alt="drag and drop icon" />
-
-              <div>
-                <p className="2xl:text-lg font-semibold mb-2">
-                  Enter in a public URL:
+                <p className="2xl:text-lg font-semibold">
+                  Drag & drop local files here, or click to select
                 </p>
-                <div className="relative w-full max-w-md">
-                  <Input
-                    className="bg-[#353535] text-[#7E7C78] rounded-full py-5 pr-16 w-full text-sm"
-                    placeholder="https://example.com/file.pdf"
-                  />
-                  <Button className="cursor-pointer absolute top-1/2 right-1 -translate-y-1/2 bg-[#ffffff27] hover:bg-neutral-500 text-white rounded-full">
-                    Add
-                  </Button>
+              </div>
+
+              {/* add url card */}
+              <div className="px-6 py-2.5 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+                <img src={UrlIcon} alt="drag and drop icon" />
+
+                <div>
+                  <p className="2xl:text-lg font-semibold mb-2">
+                    Enter in a public URL:
+                  </p>
+                  <div className="relative w-full max-w-md">
+                    <Input
+                      className="bg-[#353535] text-[#7E7C78] rounded-full py-5 pr-16 w-full text-sm"
+                      placeholder="https://example.com/file.pdf"
+                    />
+                    <Button className="cursor-pointer absolute top-1/2 right-1 -translate-y-1/2 bg-[#ffffff27] hover:bg-neutral-500 text-white rounded-full">
+                      Add
+                    </Button>
+                  </div>
                 </div>
+              </div>
+
+              {/* dropbox card */}
+              <div className="px-6 py-2.5 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+                <img src={DropBoxIcon} alt="drag and drop icon" />
+
+                <p className="2xl:text-lg font-semibold">
+                  Drag & drop local files here, or click to select
+                </p>
+              </div>
+
+              {/* google drive card */}
+              <div className="px-6 py-2.5 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+                <img src={DriveIcon} alt="drag and drop icon" />
+
+                <p className="2xl:text-lg font-semibold">
+                  Drag & drop local files here, or click to select
+                </p>
               </div>
             </div>
 
-            {/* dropbox card */}
-            <div className="px-6 py-4 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
-              <img src={DropBoxIcon} alt="drag and drop icon" />
+            {/* uploads */}
+            <div className="mt-2 2xl:mt-5 p-4 bg-[#242424] rounded-full flex items-center gap-2">
+              <div className="text-sm flex justify-between items-center gap-3.5 w-fit p-3 py-1.5 bg-[#2B2B2B] rounded-full">
+                <div className="flex items-center gap-2">
+                  <img src={PdfIcon} alt="pdf Icon" />
+                  <span>Report_file.pdf</span>
+                </div>
 
-              <p className="2xl:text-lg font-semibold">
-                Drag & drop local files here, or click to select
-              </p>
-            </div>
+                <div className="text-[#B&B7B7] flex items-center gap-2">
+                  <Checkbox className="border border-white/40" />
+                  <p>Force OCR</p>
+                  <img className="opacity-25" src={ToolTipIcon} alt="" />
+                </div>
 
-            {/* google drive card */}
-            <div className="px-6 py-4 2xl:py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
-              <img src={DriveIcon} alt="drag and drop icon" />
+                <X size={14} />
+              </div>
 
-              <p className="2xl:text-lg font-semibold">
-                Drag & drop local files here, or click to select
-              </p>
+              <div className="text-sm flex justify-between items-center gap-3.5 w-fit p-3 py-1.5 bg-[#2B2B2B] rounded-full">
+                <div className="flex items-center gap-2">
+                  <img src={PdfIcon} alt="pdf Icon" />
+                  <span>Report_file.pdf</span>
+                </div>
+
+                <div className="text-[#B&B7B7] flex items-center gap-2">
+                  <Checkbox className="border border-white/40" />
+                  <p>Force OCR</p>
+                  <img className="opacity-25" src={ToolTipIcon} alt="" />
+                </div>
+
+                <X size={14} />
+              </div>
             </div>
           </div>
 
