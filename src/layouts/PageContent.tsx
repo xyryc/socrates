b807e142673932b4c3bb77659429cc01521ffe67
xyryc <amdanikzz@gmail.com>
2025-03-08 06:59:33 +0600

    added main content cards

--- src/layouts/PageContent.tsx
+++ src/layouts/PageContent.tsx
@@ -1,18 +1,24 @@
 import { Info } from "lucide-react";
 import BackgroundPattern from "/hero_pattern.png"
+import DragNDropIcon from "/dragndrop.png"
+import UrlIcon from "/url.png"
+import DropBoxIcon from "/dropbox.png"
+import DriveIcon from "/drive.png"
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
 
 const PageContent = () => {
   return (
-    <div className="text-center relative bg-[#2B2B2B] rounded-2xl">
+    <div className="text-center relative bg-[#2B2B2B] rounded-2xl z-10">
       <img
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
         src={BackgroundPattern}
         alt="background pattern"
       />
 
       <div className="flex flex-col items-center justify-center">
         {/* info */}
-        <div className="flex items-center gap-3 mt-10">
+        <div className="flex items-center gap-3 mt-10 bg-[#242424] pl-4 pr-3 py-2 rounded-full">
           <Info className="text-[#FB9937]" />
           <p className="text-sm">
             The web version does not display local chats. To access all features,
@@ -34,7 +40,46 @@ const PageContent = () => {
           </div>
 
           {/* cards */}
-          <div></div>
+          <div className="max-w-[820px] py-[60px] grid grid-cols-2 gap-2 text-left">
+            {/* drag and drop card */}
+            <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+              <img src={DragNDropIcon} alt="drag and drop icon" />
+
+              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+            </div>
+
+            {/* add url card */}
+            <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+              <img src={UrlIcon} alt="drag and drop icon" />
+
+              <div>
+                <p className="text-lg font-semibold mb-2">Enter in a public URL:</p>
+                <div className="relative w-full max-w-md">
+                  <Input className="bg-[#353535] text-[#7E7C78] rounded-full py-5 pr-16 w-full text-sm" placeholder="https://example.com/file.pdf" />
+                  <Button className="cursor-pointer absolute top-1/2 right-1 -translate-y-1/2 bg-[#ffffff27] hover:bg-neutral-500 text-white rounded-full">
+                    Add
+                  </Button>
+                </div>
+              </div>
+            </div>
+
+
+            {/* dropbox card */}
+            <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+              <img src={DropBoxIcon} alt="drag and drop icon" />
+
+              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+            </div>
+
+            {/* google drive card */}
+            <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
+              <img src={DriveIcon} alt="drag and drop icon" />
+
+              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+            </div>
+          </div>
+
+
 
           {/* buttons */}
 
