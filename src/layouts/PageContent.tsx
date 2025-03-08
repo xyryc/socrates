629f2dbe1deeff7c35bfb0435970f3122ee1d513
xyryc <amdanikzz@gmail.com>
2025-03-08 15:56:29 +0600

    designed sidebar top

--- src/layouts/PageContent.tsx
+++ src/layouts/PageContent.tsx
@@ -1,10 +1,10 @@
 import { Info, Sparkles } from "lucide-react";
-import BackgroundPattern from "/hero_pattern.png"
-import DragNDropIcon from "/dragndrop.png"
-import UrlIcon from "/url.png"
-import DropBoxIcon from "/dropbox.png"
-import ToolTipIcon from "/Tooltip.png"
-import DriveIcon from "/drive.png"
+import BackgroundPattern from "/hero_pattern.png";
+import DragNDropIcon from "/dragndrop.png";
+import UrlIcon from "/url.png";
+import DropBoxIcon from "/dropbox.png";
+import ToolTipIcon from "/Tooltip.png";
+import DriveIcon from "/drive.png";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import {
@@ -12,7 +12,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "../components/ui/tooltip"
+} from "../components/ui/tooltip";
 
 const PageContent = () => {
   return (
@@ -23,13 +23,13 @@ const PageContent = () => {
         alt="background pattern"
       />
 
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center h-full">
         {/* info */}
         <div className="flex items-center gap-3 mt-10 bg-[#242424] pl-4 pr-3 py-2 rounded-full">
           <Info className="text-[#FB9937]" />
           <p className="text-sm">
-            The web version does not display local chats. To access all features,
-            please
+            The web version does not display local chats. To access all
+            features, please
             <span className="text-[#FB9937] font-semibold">
               {" "}
               install the app.
@@ -41,9 +41,16 @@ const PageContent = () => {
         <div className="my-20">
           {/* add files */}
           <div>
-            <p className="text-[54px] font-medium mb-6">Add Files to Cloud Chat</p>
-            <p className="text-sm">Your files will not be stored on our servers and no AI models will be trained.</p>
-            <p className="text-sm font-semibold text-[#F37F0C]">Supported File Types: .docx, .pdf, .epub, and many text filetypes</p>
+            <p className="text-[54px] font-medium mb-6">
+              Add Files to Cloud Chat
+            </p>
+            <p className="text-sm">
+              Your files will not be stored on our servers and no AI models will
+              be trained.
+            </p>
+            <p className="text-sm font-semibold text-[#F37F0C]">
+              Supported File Types: .docx, .pdf, .epub, and many text filetypes
+            </p>
           </div>
 
           {/* cards */}
@@ -52,7 +59,9 @@ const PageContent = () => {
             <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
               <img src={DragNDropIcon} alt="drag and drop icon" />
 
-              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+              <p className="text-lg font-semibold">
+                Drag & drop local files here, or click to select
+              </p>
             </div>
 
             {/* add url card */}
@@ -60,9 +69,14 @@ const PageContent = () => {
               <img src={UrlIcon} alt="drag and drop icon" />
 
               <div>
-                <p className="text-lg font-semibold mb-2">Enter in a public URL:</p>
+                <p className="text-lg font-semibold mb-2">
+                  Enter in a public URL:
+                </p>
                 <div className="relative w-full max-w-md">
-                  <Input className="bg-[#353535] text-[#7E7C78] rounded-full py-5 pr-16 w-full text-sm" placeholder="https://example.com/file.pdf" />
+                  <Input
+                    className="bg-[#353535] text-[#7E7C78] rounded-full py-5 pr-16 w-full text-sm"
+                    placeholder="https://example.com/file.pdf"
+                  />
                   <Button className="cursor-pointer absolute top-1/2 right-1 -translate-y-1/2 bg-[#ffffff27] hover:bg-neutral-500 text-white rounded-full">
                     Add
                   </Button>
@@ -74,26 +88,33 @@ const PageContent = () => {
             <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
               <img src={DropBoxIcon} alt="drag and drop icon" />
 
-              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+              <p className="text-lg font-semibold">
+                Drag & drop local files here, or click to select
+              </p>
             </div>
 
             {/* google drive card */}
             <div className="px-6 py-[27px] flex items-center gap-1.5 bg-[#242424] rounded-[20px]">
               <img src={DriveIcon} alt="drag and drop icon" />
 
-              <p className="text-lg font-semibold">Drag & drop local files here, or click to select</p>
+              <p className="text-lg font-semibold">
+                Drag & drop local files here, or click to select
+              </p>
             </div>
           </div>
-
-
 
           {/* bottom */}
           <div className="flex items-center justify-center gap-2">
             {/* buttons */}
             <div className="flex justify-center items-center gap-4 font-semibold">
-              <button className="cursor-pointer hover:bg-[#FFBF00] border border-[#FB9937] text-white px-24 py-[19px] rounded-full">Start</button>
+              <button className="cursor-pointer hover:bg-[#FFBF00] border border-[#FB9937] text-white px-24 py-[19px] rounded-full">
+                Start
+              </button>
               <span>or</span>
-              <button className="cursor-pointer py-[19px] px-[22px] flex items-center gap-2.5 bg-gradient-to-b from-[#FFBF00] to-[#FB9937] rounded-full"><Sparkles size={16} />Start with Deep Dive</button>
+              <button className="cursor-pointer py-[19px] px-[22px] flex items-center gap-2.5 bg-gradient-to-b from-[#FFBF00] to-[#FB9937] rounded-full">
+                <Sparkles size={16} />
+                Start with Deep Dive
+              </button>
             </div>
 
             {/* tooltip */}
@@ -102,17 +123,22 @@ const PageContent = () => {
                 <TooltipTrigger asChild>
                   <img src={ToolTipIcon} alt="Tool Tip Icon" />
                 </TooltipTrigger>
-                <TooltipContent align="start" side="right" className="bg-[#1E1E1E] text-xs text-white max-w-52 rounded-[20px] p-3">
-                  <p>Deep Dive processes documents section by section for "unlimited" context,
-                    enabling complete answers. Run once per chat to unlock
-                    Table AI and Prompt Loops</p>
+                <TooltipContent
+                  align="start"
+                  side="right"
+                  className="bg-[#1E1E1E] text-xs text-white max-w-52 rounded-[20px] p-3"
+                >
+                  <p>
+                    Deep Dive processes documents section by section for
+                    "unlimited" context, enabling complete answers. Run once per
+                    chat to unlock Table AI and Prompt Loops
+                  </p>
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,5 +1,38 @@
+import { PanelLeftClose, Plus, Search } from "lucide-react";
+import SocratesLogo from "/socratesLogo.png";
+
 const Sidebar = () => {
-  return <div className="border border-black">Sidebar</div>;
+  return (
+    <div className="border border-black flex flex-col justify-between h-[95vh] p-3">
+      {/* top */}
+      <div>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <img src={SocratesLogo} alt="Socrates logo" />
+            <p className="text-xl">socrates</p>
+          </div>
+
+          <div className="text-[#868686] flex items-center gap-2">
+            <Search className="size-5" />
+            <PanelLeftClose className="size-5" />
+          </div>
+        </div>
+
+        {/* create chat button */}
+        <div className="mt-8">
+          <button className="cursor-pointer font-semibold w-full flex items-center gap-1 justify-center py-2.5 border border-white rounded-full mb-3">
+            <Plus /> Add Folder
+          </button>
+          <button className="cursor-pointer font-semibold w-full flex items-center gap-1 justify-center py-2.5 rounded-full bg-[#FFFFFF26]">
+            <Plus /> Create Chat
+          </button>
+        </div>
+      </div>
+
+      {/* footer */}
+      <div>footer</div>
+    </div>
+  );
 };
 
 export default Sidebar;
